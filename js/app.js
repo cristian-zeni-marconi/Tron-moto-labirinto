a33e249d785eb01b4cd6cbc3caac0a608b920daf
cristian-zeni-marconi <cristian.zeni@marconirovereto.it>
2021-05-09 16:04:12 +0200

    aggiunto disegno motociclo

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 var canvas = document.getElementById("tron");
 var contex = canvas.getContext("2d");
 
+function drawMoto(){
+    contex.beginPath();
+    contex.rect(10, 10, 10, 10);
+    contex.fillStyle = "blue";
+    contex.fill();
+    contex.closePath();
+}
+
 function draw(){
     contex.clearRect(0, 0, canvas.width, canvas.height);
+    drawMoto();
 }
 
 draw();
